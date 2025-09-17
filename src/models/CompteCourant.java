ed61d075e3ef74291a5f14abfae2c790cc05ecc6
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-09-17 11:40:00 +0100

    Créer la classe  fille CompteCourant avec l'atribut supplémentaire, getters et setters

---
+++ src/models/CompteCourant.java
@@ -0,0 +1,19 @@
+package models;
+
+import java.util.ArrayList;
+
+public class CompteCourant extends Compte {
+    private double decouvert;
+
+    public double getDecouvert() {
+        return decouvert;
+    }
+
+    public void setDecouvert(double newDecouvert) {
+        this.decouvert = newDecouvert;
+    }
+
+
+
+}
+
