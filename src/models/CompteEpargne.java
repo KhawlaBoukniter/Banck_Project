b0ddd2f9dc4265a7911327fdcd618d33fb02692c
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-09-18 09:10:00 +0100

    remplacer les appels listeOperations.add(String) par des objets Retrait/Versement et fixer le taux d'interets

--- src/models/CompteEpargne.java
+++ src/models/CompteEpargne.java
@@ -1,34 +1,51 @@
 package models;
 
 public class CompteEpargne extends Compte {
-    private Double tauxInteret;
+    private Double tauxInteret = 0.2;
 
     public Double getTauxInteret() {
         return tauxInteret;
     }
 
-    public void setTauxInteret(Double newTauxInteret) {
-        this.tauxInteret = newTauxInteret;
-    }
+//    public void setTauxInteret(Double newTauxInteret) {
+//        this.tauxInteret = newTauxInteret;
+//    }
 
-    public boolean retirer(Double montant) {
+    public Boolean retirer(Double montant) {
         if (montant <= solde) {
             solde -= montant;
-            listeOperations.add("Retrait de: " + montant);
+            listeOperations.add(new Retrait(montant));
             return true;
         } else {
             return false;
         }
     }
 
-    public boolean verser(Double montant) {
-        if (montant > 0) {
-            solde += montant;
-            listeOperations.add("versement de: " + montant);
+    public Boolean retirer(Double montant, String destination) {
+        if (montant <= solde) {
+            solde -= montant;
+            listeOperations.add(new Retrait(montant, destination));
             return true;
         }
         return false;
+    }
 
+    public Boolean verser(Double montant) {
+        if (montant > 0) {
+            solde += montant;
+            listeOperations.add(new Versement(montant));
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean verser(Double montant, String source) {
+        if (montant > 0) {
+            solde += montant;
+            listeOperations.add(new Versement(source, montant));
+            return true;
+        }
+        return false;
     }
 
     public Double calculerInteret() {
