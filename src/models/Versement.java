67c2188551b3299a4e9005c3bd8a5562dab68810
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-09-19 10:17:55 +0100

    ajuster le message de retrait pour inclure la source et date uniquement si présents

--- src/models/Versement.java
+++ src/models/Versement.java
@@ -1,4 +1,24 @@
 package models;
 
-public class Versement {
+public class Versement extends Operation {
+    private String source;
+
+    public Versement (String source, Double montant) {
+        super(montant);
+        this.source = source;
+    }
+
+    public Versement(Double montant) {
+        super(montant);
+    }
+
+    @Override
+    public String getOperation() {
+        if (source == null) {
+            return getNumero() + " : Versement de " + getMontant();
+        } else {
+            return getNumero() + " : Versement de " + getMontant() + " depuis " + source + " le " + getDate();
+        }
+
+    }
 }
